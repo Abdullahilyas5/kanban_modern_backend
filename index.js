f8e934b5e31dbf9deab7e438a29111c2dd4bb98f
Abdullah <abdullah.ilyas@alphahiveai.com>
2026-05-14 12:48:39 +0500

    update the cors config

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_ORIGIN || "http://localhost:3000",
+    origin: process.env.CLIENT_ORIGIN,
     credentials: true,
   })
 );
