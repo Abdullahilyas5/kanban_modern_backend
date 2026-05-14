5243754de5830c5b102a0daa2ab9864859d9e906
Abdullah <abdullah.ilyas@alphahiveai.com>
2026-05-14 12:35:10 +0500

    update the configuration

--- index.js
+++ index.js
@@ -9,26 +9,32 @@ import taskRoutes from "./routes/task.routes.js";
 dotenv.config();
 
 const app = express();
-app.use(cors({ origin: process.env.CLIENT_ORIGIN || "http://localhost:3000", credentials: true }));
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_ORIGIN || "http://localhost:3000",
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
-const PORT = process.env.PORT || 5000;
 
 app.use("/users", userRoutes);
 app.use("/boards", boardRoutes);
 app.use("/tasks", taskRoutes);
 
-
-app.get("/health", (req, res)=>{
-    return res.status(200).json({
-        status : "success",
-        message : "Server is healthy"
-    });
-})
-
-
-app.listen(PORT , ()=>{
-    console.log(`Server is running on port ${PORT}`);
+app.get("/health", (req, res) => {
+  return res.status(200).json({
+    status: "success",
+    message: "Server is healthy",
+  });
 });
+
+
+// app.listen(process.env.PORT || 5000, () => {
+//   console.log(`Server is running on port ${process.env.PORT || 5000}`);
+// });
+
+export default app;
